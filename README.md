fea28714aaf74ce6b8fe16d4115771878d8cfd8a
NIKHILSAWANT07 <nikhilsawant2864@gmail.com>
2024-07-12 23:40:41 +0530

    commit 3.5

--- README.md
+++
@@ -1,1 +0,0 @@
-# Baby-Vaccination-React-Website
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
+
 import App from "App";
 
 // Soft UI Dashboard React Context Provider
@@ -10,10 +12,10 @@ import { SoftUIControllerProvider } from "context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <SoftUIControllerProvider>
       
       <App />
     </SoftUIControllerProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
